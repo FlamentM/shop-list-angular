8cdbe5c754ddfad83c62be25aa417694a2bc5ca1
Barzou <flament.mickael@gmail.com>
2015-10-04 21:24:11 +0200

    Première ébauche

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,44 @@
 'use strict';
 
-var app = angular.module("myApp", []);
+var app = angular.module("myApp", ['ngMaterial']);
 
-app.controller("headerCtrl", function($scope){
+/*
+  les objets seront sous forme : {
+    list_label = "nom de la list",
+    label = ""
+  }
+
+ */
+
+
+app.controller("listCtrl", ['$scope', '$mdSidenav', 'firebaseService', function($scope, $mdSidenav, firebaseService){
   $scope.headerName = "Le nom dans le header";
-});
+  $scope.list = [];
+  $scope.listLabel= [
+    "All",
+    "Others",
+    "Fruits",
+    "Légumes"
+  ];
+  $scope.currentList = "Others";
 
-app.controller("footerCtrl", function($scope){
-  //...
-});
+  $scope.toggleSidenav = function(menuId) {
+    $mdSidenav(menuId).toggle();
+  };
 
-app.controller("menuCtrl", function($scope){
-  //...
-});
 
-app.controller("contentCtrl", function($scope){
-  //...
-});
+  $scope.changeCurrentList = function($event){
+    $scope.currentList = $event.target.innerText;
+  };
+
+  firebaseService.setProcessingFunction(function(snapshot){
+    $scope.list.push({
+      "value" : snapshot.val(),
+      "snapshot" : snapshot
+    });
+  });
+
+  $scope.send = function(){
+    
+  };
+}]);
