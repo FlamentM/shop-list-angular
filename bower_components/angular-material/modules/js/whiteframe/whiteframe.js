9ae35c08e524f9e885e97172e4b4ceddb46ddcdb
Barzou <flament.mickael@gmail.com>
2015-10-11 17:10:21 +0200

    Angular shop list v0.1
    Ne gère pas encore l'ajout de liste. Gère l'ajout, la suppression et la
    persistance des produits.

---
+++ bower_components/angular-material/modules/js/whiteframe/whiteframe.js
@@ -0,0 +1,16 @@
+/*!
+ * Angular Material Design
+ * https://github.com/angular/material
+ * @license MIT
+ * v0.11.2-master-b998696
+ */
+(function( window, angular, undefined ){
+"use strict";
+
+/**
+ * @ngdoc module
+ * @name material.components.whiteframe
+ */
+angular.module('material.components.whiteframe', []);
+
+})(window, window.angular);
---
+++ view-marmitton/marmitton.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('app.marmitton', ['app.firebase-services'])
+    .controller('MarmittonController', function () {
+        var marmittonCtrl = this;
+    });
+
